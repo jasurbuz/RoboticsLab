40f6cca1310257fd5971a9f80de360e1eac1fb75
karimberdiyev <87532063+karimberdiyev@users.noreply.github.com>
2021-07-16 19:46:54 +0500

    updated

--- Backend/Human.cs
+++ Backend/Human.cs
@@ -8,5 +8,12 @@ namespace Backend
 {
     class Human
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public void github()
+        {
+            Console.WriteLine("Guys Let's start");
+        }
     }
 }
