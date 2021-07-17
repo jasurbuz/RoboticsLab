6a1c2c358dee393ba55802cff4f2d9a5c3ca78f0
jasurbuz <81855769+jasurbuz@users.noreply.github.com>
2021-07-17 16:06:53 +0500

    Added Student model

---
+++ Backend/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    class StudentModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
