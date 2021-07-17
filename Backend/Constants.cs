ec9a607c28166a772b2302cdf149db24b2549ded
jasurbuz <81855769+jasurbuz@users.noreply.github.com>
2021-07-17 15:51:22 +0500

    Added packages

--- Backend/Constants.cs
+++ Backend/Constants.cs
@@ -8,5 +8,8 @@ namespace Backend
 {
     class Constants
     {
+        public readonly static string connectString = "Server=ec2-3-231-69-204.compute-1.amazonaws.com; Port=5432; " +
+            "Database=d2p4sstuir7ikf; User Id = lqehwaufwrwyop; Password=07d5c035e54c790c1e3b43ad3321f5e3a7af9162e33c161e61e00438af57950a;" +
+            "Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True;";
     }
 }
