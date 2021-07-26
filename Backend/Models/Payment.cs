025c5f7a773a121a2b899d13bb86df2fc715bd80
karimberdiyev <87532063+karimberdiyev@users.noreply.github.com>
2021-07-26 13:40:51 +0500

    +

--- Backend/Models/Payment.cs
+++ Backend/Models/Payment.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Backend.Models
 {
+    [Table("face")]
     class Payment
     {
+        [Key]
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
     }
 }
